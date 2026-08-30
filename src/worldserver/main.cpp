4f3bc2336ed13ec99aaa1e796a7596652af3f3fc
ghostpast <ghostpast@users.noreply.github.com>
2026-08-30 20:07:27 +0500

    Подключен модуль Asio

--- src/worldserver/main.cpp
+++ src/worldserver/main.cpp
@@ -4,14 +4,40 @@
 */
 
 #include <iostream>
+#include <asio.hpp>
 #include "revision.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main() {
+    
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+    
     std::cout << "UnrealCore WorldServer" << std::endl;
+    std::cout << std::endl;
     std::cout << "Revision: " << REVISION_ID << std::endl;
     std::cout << "Branch:   " << REVISION_BRANCH << std::endl;
     std::cout << "Date:     " << REVISION_DATE << std::endl;
     std::cout << "Built:    " << REVISION_TIME << std::endl;
- 
+    std::cout << std::endl;
+    
+    asio::io_context io_context;
+    
+    // Ждём Ctrl+C или сигнал завершения
+    asio::signal_set signals(io_context, SIGINT, SIGTERM);
+    signals.async_wait([&](const std::error_code&, int) {
+        std::cout << "\nЗавершение работы..." << std::endl;
+        io_context.stop();
+    });
+    
+    std::cout << "Для остановки нажмите Ctrl+C." << std::endl;
+    
+    io_context.run(); // блокирует поток, пока io_context не остановлен
+    
     return 0;
 }
