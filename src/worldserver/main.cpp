bfe7f87e1bef18192734ef240f91ac4d5bfa9540
ghostpast <ghostpast@users.noreply.github.com>
2026-08-30 18:11:58 +0500

    Добавлены файлы проекта

---
+++ src/worldserver/main.cpp
@@ -0,0 +1,17 @@
+/*
+* Copyright (C) 2026 Unreal Core
+* Условия использования указаны в файле LICENSE.md в корне проекта.
+*/
+
+#include <iostream>
+#include "revision.h"
+
+int main() {
+    std::cout << "UnrealCore WorldServer" << std::endl;
+    std::cout << "Revision: " << REVISION_ID << std::endl;
+    std::cout << "Branch:   " << REVISION_BRANCH << std::endl;
+    std::cout << "Date:     " << REVISION_DATE << std::endl;
+    std::cout << "Built:    " << REVISION_TIME << std::endl;
+ 
+    return 0;
+}
